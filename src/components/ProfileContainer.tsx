50d67123f349d9bb17f0e393c4b68e435e3c08c3
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2024-07-28 22:00:41 -0300

    feat: download curriculum

--- src/components/ProfileContainer.tsx
+++ src/components/ProfileContainer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import styled from "styled-components";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-import circle from "../assets/tony.png"
+import circle from "../assets/tony.png";
+import DefaultButton from "./DefaultButton";
 
 const ProfileContainerContainer = styled.div`
   text-align: center;
@@ -15,12 +15,12 @@ const ProfileContainerContainer = styled.div`
 const ProfileImage = styled.img`
   border-radius: 50%;
   width: 300px;
-  border: 7px outset ${({theme})=> theme.colors.text};
-`
+  border: 7px outset ${({ theme }) => theme.colors.text};
+`;
 
 const IconContainer = styled.div`
   display: inline-block;
-  margin: 0 10px;
+  margin: 10px;
   cursor: pointer;
   transition: transform 0.3s ease;
 
@@ -30,26 +30,37 @@ const IconContainer = styled.div`
 `;
 
 export default function ProfileContainer() {
-  const handleLinkedInClick = () => {
-    window.open('https://www.linkedin.com/in/antonio-mosquera/', '_blank');
-  };
+  function handleLinkedInClick() {
+    window.open("https://www.linkedin.com/in/antonio-mosquera/", "_blank");
+  }
 
-  const handleGitHubClick = () => {
-    window.open('https://github.com/TonyFilgueiras', '_blank');
-  };
+  function handleGitHubClick() {
+    window.open("https://github.com/TonyFilgueiras", "_blank");
+  }
+
+  function handleDownloadClick() {
+    const link = document.createElement("a");
+    link.href = "/resume.pdf"; // Replace with the correct path to your PDF file
+    link.download = "Tony_Filgueiras_Resume.pdf"; // The name of the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
   return (
     <ProfileContainerContainer>
       <h1>Tony Filgueiras</h1>
       <h2>Fullstack Developer</h2>
-      <ProfileImage src={circle}/><br/>
+      <ProfileImage src={circle} />
+      <br />
       <IconContainer onClick={handleLinkedInClick} title="Linkedin">
         <LinkedInIcon fontSize="large" />
       </IconContainer>
       <IconContainer onClick={handleGitHubClick} title="GitHub">
         <GitHubIcon fontSize="large" />
-      </IconContainer><br/>
-      <button>Download Resume</button>
+      </IconContainer>
+      <br />
+      <DefaultButton text="Download Curriculum" onClick={handleDownloadClick} />
     </ProfileContainerContainer>
   );
 }
